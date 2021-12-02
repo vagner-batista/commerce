f66d10af9575863cb4dfa5a38327f4575cc37f3b
Vagner Batista <vagnereletronica@gmail.com>
2021-12-02 12:17:04 -0300

    Adicionadas bibliotecas

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import React, { useEffect } from 'react';
+import { StoreProvider } from '../utils/Store';
+import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    //Remove serverside styles
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <Provider session={pageProps.session}>
+      <StoreProvider>
+        <Component {...pageProps} />;
+      </StoreProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
